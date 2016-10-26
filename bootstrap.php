897d7dd2aed394dc7b1a2a50b5e8a468191c087f
Daniel Klabbers <daniel@productip.com>
2016-10-26 16:35:16 +0200

    first commit

---
+++ bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of flagrow/flarum-ext-file-upload.
+ *
+ * Copyright (c) Flagrow.
+ *
+ * http://flagrow.github.io
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Flagrow\Upload;
+
+use Flarum\Foundation\Application;
+use Illuminate\Contracts\Events\Dispatcher;
+
+return function (Dispatcher $events, Application $app) {
+};
