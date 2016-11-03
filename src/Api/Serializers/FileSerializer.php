3de7f6d3890a1702ca55acf2bdd95dd2f3aae7d6
Daniel Klabbers <daniel@productip.com>
2016-11-03 16:54:23 +0100

    id is already sent by default

--- src/Api/Serializers/FileSerializer.php
+++ src/Api/Serializers/FileSerializer.php
@@ -32,7 +32,7 @@ class FileSerializer extends AbstractSerializer
     {
         return Arr::only(
             $model->attributesToArray(),
-            ['id', 'base_name', 'path', 'url', 'type', 'size']
+            ['base_name', 'path', 'url', 'type', 'size']
         );
     }
 }
