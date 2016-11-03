e38230622e46cd5e02ec6418a86ab2e2551304e4
Daniel Klabbers <daniel@productip.com>
2016-11-03 13:28:02 +0100

    added less and compiled js

---
+++ js/admin/dist/extension.js
@@ -0,0 +1,260 @@
+"use strict";
+
+System.register("flagrow/upload/addUploadPane", ["flarum/extend", "flarum/components/AdminNav", "flarum/components/AdminLinkButton", "flagrow/upload/components/UploadPage"], function (_export, _context) {
+    "use strict";
+
+    var extend, AdminNav, AdminLinkButton, UploadPage;
+
+    _export("default", function () {
+        // create the route
+        app.routes['flagrow-upload'] = {path: '/flagrow/upload', component: UploadPage.component()};
+
+        // bind the route we created to the three dots settings button
+        app.extensionSettings['flagrow-upload'] = function () {
+            return m.route(app.route('flagrow-upload'));
+        };
+
+        extend(AdminNav.prototype, 'items', function (items) {
+            // add the Image Upload tab to the admin navigation menu
+            items.add('flagrow-upload', AdminLinkButton.component({
+                href: app.route('flagrow-upload'),
+                icon: 'file-o',
+                children: 'File Upload',
+                description: app.translator.trans('flagrow-upload.admin.help_texts.description')
+            }));
+        });
+    });
+
+    return {
+        setters: [function (_flarumExtend) {
+            extend = _flarumExtend.extend;
+        }, function (_flarumComponentsAdminNav) {
+            AdminNav = _flarumComponentsAdminNav.default;
+        }, function (_flarumComponentsAdminLinkButton) {
+            AdminLinkButton = _flarumComponentsAdminLinkButton.default;
+        }, function (_flagrowUploadComponentsUploadPage) {
+            UploadPage = _flagrowUploadComponentsUploadPage.default;
+        }],
+        execute: function () {
+        }
+    };
+});
+;
+"use strict";
+
+System.register("flagrow/upload/components/UploadPage", ["flarum/Component", "flarum/components/Button", "flarum/utils/saveSettings", "flarum/components/Alert", "flarum/components/FieldSet", "flarum/components/Select", "flarum/components/Switch"], function (_export, _context) {
+    "use strict";
+
+    var Component, Button, saveSettings, Alert, FieldSet, Select, Switch, UploadPage;
+    return {
+        setters: [function (_flarumComponent) {
+            Component = _flarumComponent.default;
+        }, function (_flarumComponentsButton) {
+            Button = _flarumComponentsButton.default;
+        }, function (_flarumUtilsSaveSettings) {
+            saveSettings = _flarumUtilsSaveSettings.default;
+        }, function (_flarumComponentsAlert) {
+            Alert = _flarumComponentsAlert.default;
+        }, function (_flarumComponentsFieldSet) {
+            FieldSet = _flarumComponentsFieldSet.default;
+        }, function (_flarumComponentsSelect) {
+            Select = _flarumComponentsSelect.default;
+        }, function (_flarumComponentsSwitch) {
+            Switch = _flarumComponentsSwitch.default;
+        }],
+        execute: function () {
+            UploadPage = function (_Component) {
+                babelHelpers.inherits(UploadPage, _Component);
+
+                function UploadPage() {
+                    babelHelpers.classCallCheck(this, UploadPage);
+                    return babelHelpers.possibleConstructorReturn(this, (UploadPage.__proto__ || Object.getPrototypeOf(UploadPage)).apply(this, arguments));
+                }
+
+                babelHelpers.createClass(UploadPage, [{
+                    key: "init",
+                    value: function init() {
+                        var _this2 = this;
+
+                        // whether we are saving the settings or not right now
+                        this.loading = false;
+
+                        // the fields we need to watch and to save
+                        this.fields = ['availableUploadMethods', 'uploadMethod', 'resizeMaxWidth', 'resizeMaxHeight', 'cdnUrl', 'maxFileSize'];
+
+                        // the checkboxes we need to watch and to save.
+                        this.checkboxes = ['mustResize'];
+
+                        // options for the dropdown menu
+                        this.uploadMethodOptions = {};
+
+                        this.values = {};
+
+                        // our package prefix (to be added to every field and checkbox in the setting table)
+                        this.settingsPrefix = 'flagrow.upload';
+
+                        // get the saved settings from the database
+                        var settings = app.data.settings;
+
+                        // set the upload methods
+                        this.uploadMethodOptions = settings[this.addPrefix('availableUploadMethods')];
+                        // bind the values of the fields and checkboxes to the getter/setter functions
+                        this.fields.forEach(function (key) {
+                            return _this2.values[key] = m.prop(settings[_this2.addPrefix(key)]);
+                        });
+                        this.checkboxes.forEach(function (key) {
+                            return _this2.values[key] = m.prop(settings[_this2.addPrefix(key)] === '1');
+                        });
+                    }
+                }, {
+                    key: "view",
+                    value: function view() {
+                        return [m('div', {className: 'ImageUploadPage'}, [m('div', {className: 'container'}, [m('form', {onsubmit: this.onsubmit.bind(this)}, [FieldSet.component({
+                            label: app.translator.trans('flagrow.upload.admin.labels.upload_method'),
+                            children: [m('div', {className: 'helpText'}, app.translator.trans('flagrow.upload.admin.help_texts.upload_method')), Select.component({
+                                options: this.uploadMethodOptions,
+                                onchange: this.values.uploadMethod,
+                                value: this.values.uploadMethod() || 'local'
+                            })]
+                        }), m('div', {className: 'ImageUploadPage-preferences'}, [FieldSet.component({
+                            label: app.translator.trans('flagrow.upload.admin.labels.preferences.title'),
+                            children: [m('label', {}, app.translator.trans('flagrow.upload.admin.labels.preferences.max_file_size')), m('input', {
+                                className: 'FormControl',
+                                value: this.values.maxFileSize() || 2048,
+                                oninput: m.withAttr('value', this.values.maxFileSize)
+                            })]
+                        })]), m('div', {className: 'ImageUploadPage-resize'}, [FieldSet.component({
+                            label: app.translator.trans('flagrow.upload.admin.labels.resize.title'),
+                            children: [m('div', {className: 'helpText'}, app.translator.trans('flagrow.upload.admin.help_texts.resize')), Switch.component({
+                                state: this.values.mustResize() || false,
+                                children: app.translator.trans('flagrow.upload.admin.labels.resize.toggle'),
+                                onchange: this.values.mustResize
+                            }), m('label', {}, app.translator.trans('flagrow.upload.admin.labels.resize.max_width')), m('input', {
+                                className: 'FormControl',
+                                value: this.values.resizeMaxWidth() || 100,
+                                oninput: m.withAttr('value', this.values.resizeMaxWidth),
+                                disabled: !this.values.mustResize()
+                            }), m('label', {}, app.translator.trans('flagrow.upload.admin.labels.resize.max_height')), m('input', {
+                                className: 'FormControl',
+                                value: this.values.resizeMaxHeight() || 100,
+                                oninput: m.withAttr('value', this.values.resizeMaxHeight),
+                                disabled: !this.values.mustResize()
+                            })]
+                        })]), m('div', {
+                            className: 'ImageUploadPage-local',
+                            style: {display: this.values.uploadMethod() === 'local' ? "block" : "none"}
+                        }, [FieldSet.component({
+                            label: app.translator.trans('flagrow.upload.admin.labels.local.title'),
+                            children: [m('label', {}, app.translator.trans('flagrow.upload.admin.labels.local.cdn_url')), m('input', {
+                                className: 'FormControl',
+                                value: this.values.cdnUrl() || '',
+                                oninput: m.withAttr('value', this.values.cdnUrl)
+                            })]
+                        })]), Button.component({
+                            type: 'submit',
+                            className: 'Button Button--primary',
+                            children: app.translator.trans('flagrow.upload.admin.buttons.save'),
+                            loading: this.loading,
+                            disabled: !this.changed()
+                        })])])])];
+                    }
+                }, {
+                    key: "changed",
+                    value: function changed() {
+                        var _this3 = this;
+
+                        var fieldsCheck = this.fields.some(function (key) {
+                            return _this3.values[key]() !== app.data.settings[_this3.addPrefix(key)];
+                        });
+                        var checkboxesCheck = this.checkboxes.some(function (key) {
+                            return _this3.values[key]() !== (app.data.settings[_this3.addPrefix(key)] == '1');
+                        });
+                        return fieldsCheck || checkboxesCheck;
+                    }
+                }, {
+                    key: "onsubmit",
+                    value: function onsubmit(e) {
+                        var _this4 = this;
+
+                        // prevent the usual form submit behaviour
+                        e.preventDefault();
+
+                        // if the page is already saving, do nothing
+                        if (this.loading) return;
+
+                        // prevents multiple savings
+                        this.loading = true;
+                        app.alerts.dismiss(this.successAlert);
+
+                        var settings = {};
+
+                        // gets all the values from the form
+                        this.fields.forEach(function (key) {
+                            return settings[_this4.addPrefix(key)] = _this4.values[key]();
+                        });
+                        this.checkboxes.forEach(function (key) {
+                            return settings[_this4.addPrefix(key)] = _this4.values[key]();
+                        });
+
+                        // actually saves everything in the database
+                        saveSettings(settings).then(function () {
+                            // on succes, show an alert
+                            app.alerts.show(_this4.successAlert = new Alert({
+                                type: 'success',
+                                children: app.translator.trans('core.admin.basics.saved_message')
+                            }));
+                        }).catch(function () {
+                        }).then(function () {
+                            // return to the initial state and redraw the page
+                            _this4.loading = false;
+                            m.redraw();
+                        });
+                    }
+                }, {
+                    key: "addPrefix",
+                    value: function addPrefix(key) {
+                        return this.settingsPrefix + '.' + key;
+                    }
+                }]);
+                return UploadPage;
+            }(Component);
+
+            _export("default", UploadPage);
+        }
+    };
+});
+;
+"use strict";
+
+System.register("flagrow/upload/main", ["flarum/extend", "flarum/app", "flarum/components/PermissionGrid", "flagrow/upload/addUploadPane"], function (_export, _context) {
+    "use strict";
+
+    var extend, app, PermissionGrid, addUploadPane;
+    return {
+        setters: [function (_flarumExtend) {
+            extend = _flarumExtend.extend;
+        }, function (_flarumApp) {
+            app = _flarumApp.default;
+        }, function (_flarumComponentsPermissionGrid) {
+            PermissionGrid = _flarumComponentsPermissionGrid.default;
+        }, function (_flagrowUploadAddUploadPane) {
+            addUploadPane = _flagrowUploadAddUploadPane.default;
+        }],
+        execute: function () {
+
+            app.initializers.add('flagrow-upload', function (app) {
+                // add the admin pane
+                addUploadPane();
+
+                // add the permission option to the relative pane
+                extend(PermissionGrid.prototype, 'startItems', function (items) {
+                    items.add('upload', {
+                        icon: 'file-o',
+                        label: app.translator.trans('flagrow-upload.admin.permissions.upload_images_label'),
+                        permission: 'flagrow.upload'
+                    });
+                });
+            });
+        }
+    };
+});
---
+++ js/forum/dist/extension.js
@@ -0,0 +1,149 @@
+"use strict";
+
+System.register("flagrow/upload/components/UploadButton", ["flarum/Component", "flarum/helpers/icon", "flarum/components/LoadingIndicator"], function (_export, _context) {
+    "use strict";
+
+    var Component, icon, LoadingIndicator, UploadButton;
+    return {
+        setters: [function (_flarumComponent) {
+            Component = _flarumComponent.default;
+        }, function (_flarumHelpersIcon) {
+            icon = _flarumHelpersIcon.default;
+        }, function (_flarumComponentsLoadingIndicator) {
+            LoadingIndicator = _flarumComponentsLoadingIndicator.default;
+        }],
+        execute: function () {
+            UploadButton = function (_Component) {
+                babelHelpers.inherits(UploadButton, _Component);
+
+                function UploadButton() {
+                    babelHelpers.classCallCheck(this, UploadButton);
+                    return babelHelpers.possibleConstructorReturn(this, (UploadButton.__proto__ || Object.getPrototypeOf(UploadButton)).apply(this, arguments));
+                }
+
+                babelHelpers.createClass(UploadButton, [{
+                    key: "init",
+                    value: function init() {
+                        // the service type handling uploads
+                        this.textAreaObj = null;
+
+                        // initial state of the button
+                        this.loading = false;
+                    }
+                }, {
+                    key: "view",
+                    value: function view() {
+                        return m('div', {className: 'Button hasIcon flagrow-upload-button Button--icon'}, [this.loading ? LoadingIndicator.component({className: 'Button-icon'}) : icon('file-o', {className: 'Button-icon'}), m('span', {className: 'Button-label'}, this.loading ? app.translator.trans('flagrow-upload.forum.states.loading') : app.translator.trans('flagrow-upload.forum.buttons.attach')), m('form#flagrow-upload-form', [m('input', {
+                            type: 'file',
+                            name: 'flagrow-upload-input',
+                            onchange: this.process.bind(this)
+                        })])]);
+                    }
+                }, {
+                    key: "process",
+                    value: function process(e) {
+                        // get the file from the input field
+                        var data = new FormData();
+                        data.append('file', $(e.target)[0].files[0]);
+
+                        // set the button in the loading state (and redraw the element!)
+                        this.loading = true;
+                        m.redraw();
+
+                        // send a POST request to the api
+                        app.request({
+                            method: 'POST',
+                            url: app.forum.attribute('apiUrl') + '/flagrow/upload',
+                            serialize: function serialize(raw) {
+                                return raw;
+                            },
+                            data: data
+                        }).then(this.success.bind(this), this.failure.bind(this));
+                    }
+                }, {
+                    key: "failure",
+                    value: function failure(message) {
+                    }
+                    // todo show popup
+
+
+                    /**
+                     * Appends the image's link to the body of the composer.
+                     *
+                     * @param image
+                     */
+
+                }, {
+                    key: "success",
+                    value: function success(file) {
+                        console.log(file);
+                        //
+                        // var link = image.data.attributes.url;
+                        //
+                        // // create a markdown string that holds the image link
+                        // var markdownString = '\n![image ' + link + '](' + link + ')\n';
+                        //
+                        // // place the Markdown image link in the Composer
+                        // this.textAreaObj.insertAtCursor(markdownString);
+                        //
+                        // // if we are not starting a new discussion, the variable is defined
+                        // if (typeof this.textAreaObj.props.preview !== 'undefined') {
+                        //     // show what we just uploaded
+                        //     this.textAreaObj.props.preview();
+                        // }
+                        //
+                        // // reset the button for a new upload
+                        // setTimeout(() => {
+                        //     document.getElementById("flagrow-image-upload-form").reset();
+                        //     this.loading = false;
+                        // }, 1000);
+                    }
+                }]);
+                return UploadButton;
+            }(Component);
+
+            _export("default", UploadButton);
+        }
+    };
+});
+;
+"use strict";
+
+System.register("flagrow/upload/main", ["flarum/extend", "flarum/components/TextEditor", "flagrow/upload/components/UploadButton"], function (_export, _context) {
+    "use strict";
+
+    var extend, TextEditor, UploadButton;
+    return {
+        setters: [function (_flarumExtend) {
+            extend = _flarumExtend.extend;
+        }, function (_flarumComponentsTextEditor) {
+            TextEditor = _flarumComponentsTextEditor.default;
+        }, function (_flagrowUploadComponentsUploadButton) {
+            UploadButton = _flagrowUploadComponentsUploadButton.default;
+        }],
+        execute: function () {
+
+            app.initializers.add('flagrow-upload', function (app) {
+                extend(TextEditor.prototype, 'controlItems', function (items) {
+                    // check whether the user can upload images. If not, returns.
+                    if (!app.forum.attribute('canUpload')) return;
+
+                    // create and add the button
+                    var uploadButton = new UploadButton();
+                    uploadButton.textAreaObj = this;
+                    items.add('flagrow-upload', uploadButton, 0);
+
+                    // animate the button on hover: shows the label
+                    $('.Button-label', '.item-flagrow-upload > div').hide();
+                    $('.item-flagrow-upload > div').hover(function () {
+                        $('.Button-label', this).show();
+                        $(this).removeClass('Button--icon');
+                    }, function () {
+                        $('.Button-label', this).hide();
+                        $(this).addClass('Button--icon');
+                    });
+                });
+            });
+        }
+    };
+});
