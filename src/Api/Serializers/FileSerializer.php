8b8c2aa168f40b0ce38cc1a8ca949f9d587dea5a
Daniel Klabbers <daniel+hyn@klabbers.email>
2016-11-07 21:07:04 +0100

    the serializer is ran on all objects

--- src/Api/Serializers/FileSerializer.php
+++ src/Api/Serializers/FileSerializer.php
@@ -17,7 +17,6 @@ namespace Flagrow\Upload\Api\Serializers;
 use Flagrow\Upload\File;
 use Flarum\Api\Serializer\AbstractSerializer;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Collection;
 
 class FileSerializer extends AbstractSerializer
 {
@@ -26,17 +25,14 @@ class FileSerializer extends AbstractSerializer
     /**
      * Get the default set of serialized attributes for a model.
      *
-     * @param Collection $collection
+     * @param File $model
      * @return array
      */
-    protected function getDefaultAttributes($collection)
+    protected function getDefaultAttributes($model)
     {
-        return $collection->map(function (File $file) {
-            return Arr::only(
-                $file->attributesToArray(),
-                ['base_name', 'path', 'url', 'type', 'size', 'markdownString']
-
-            );
-        })->toArray();
+        return Arr::only(
+            $model->attributesToArray(),
+            ['base_name', 'path', 'url', 'type', 'size', 'markdownString']
+        );
     }
 }
