5d71a3aa8b76585577daca68700f17ad77441a20
Davis <connor0095@gmail.com>
2017-01-05 08:04:40 +0000

    Add OVH Support (w/ Webeindustry's Assistance)

--- js/admin/src/components/UploadPage.js
+++ js/admin/src/components/UploadPage.js
@@ -33,6 +33,12 @@ export default class UploadPage extends Component {
             'awsS3Secret',
             'awsS3Bucket',
             'awsS3Region',
+            // OVH
+            'ovhUsername',
+            'ovhPassword',
+            'ovhTenantId',
+            'ovhContainer',
+            'ovhRegion',
         ];
 
         // the checkboxes we need to watch and to save.
@@ -201,6 +207,42 @@ export default class UploadPage extends Component {
                                 className: 'FormControl',
                                 value: this.values.awsS3Region() || '',
                                 oninput: m.withAttr('value', this.values.awsS3Region)
+                            }),
+                        ]),
+                        m('fieldset', {
+                            className: 'UploadPage-ovh-svfs',
+                            style: {display: (this.values.uploadMethod() === 'ovh-svfs' ? "block" : "none")}
+                        }, [
+                            m('legend', {}, app.translator.trans('flagrow-upload.admin.labels.ovh-svfs.title')),
+                            m('label', {}, app.translator.trans('flagrow-upload.admin.labels.ovh-svfs.container')),
+                            m('input', {
+                                className: 'FormControl',
+                                value: this.values.ovhContainer() || '',
+                                oninput: m.withAttr('value', this.values.ovhContainer)
+                            }),
+                            m('label', {}, app.translator.trans('flagrow-upload.admin.labels.ovh-svfs.tenantid')),
+                            m('input', {
+                                className: 'FormControl',
+                                value: this.values.ovhTenantId() || '',
+                                oninput: m.withAttr('value', this.values.ovhTenantId)
+                            }),
+                            m('label', {}, app.translator.trans('flagrow-upload.admin.labels.ovh-svfs.username')),
+                            m('input', {
+                                className: 'FormControl',
+                                value: this.values.ovhUsername() || '',
+                                oninput: m.withAttr('value', this.values.ovhUsername)
+                            }),
+                            m('label', {}, app.translator.trans('flagrow-upload.admin.labels.ovh-svfs.password')),
+                            m('input', {
+                                className: 'FormControl',
+                                value: this.values.ovhPassword() || '',
+                                oninput: m.withAttr('value', this.values.ovhPassword)
+                            }),
+                            m('label', {}, app.translator.trans('flagrow-upload.admin.labels.ovh-svfs.region')),
+                            m('input', {
+                                className: 'FormControl',
+                                value: this.values.ovhRegion() || '',
+                                oninput: m.withAttr('value', this.values.ovhRegion)
                             }),
                         ]),
                         Button.component({
