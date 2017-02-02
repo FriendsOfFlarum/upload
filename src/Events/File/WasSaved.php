f4fbf23adfcdf5e135f84eee1705a77271075767
Daniel Klabbers <daniel@productip.com>
2017-02-02 16:02:34 +0100

    Allowing adapter to be overridden using the adapter identified event.

--- src/Events/File/WasSaved.php
+++ src/Events/File/WasSaved.php
@@ -14,20 +14,6 @@
 
 namespace Flagrow\Upload\Events\File;
 
-use Flagrow\Upload\File;
-use Flarum\Core\User;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-
-class WasSaved
+class WasSaved extends Event
 {
-    public $actor;
-    public $file;
-    public $uploadedFile;
-
-    public function __construct(User $actor, File $file, UploadedFile $uploadedFile)
-    {
-        $this->actor = $actor;
-        $this->file = $file;
-        $this->uploadedFile = $uploadedFile;
-    }
 }
--- src/Events/File/WillBeUploaded.php
+++ src/Events/File/WillBeUploaded.php
@@ -14,20 +14,6 @@
 
 namespace Flagrow\Upload\Events\File;
 
-use Flagrow\Upload\File;
-use Flarum\Core\User;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-
-class WillBeUploaded
+class WillBeUploaded extends Event
 {
-    public $actor;
-    public $file;
-    public $uploadedFile;
-
-    public function __construct(User $actor, File $file, UploadedFile $uploadedFile)
-    {
-        $this->actor = $actor;
-        $this->file = $file;
-        $this->uploadedFile = $uploadedFile;
-    }
 }
