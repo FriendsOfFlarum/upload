d72f3b7a66f81e9123f077d44c76a4f68fa94eba
Clark Winkelmann <clark.winkelmann@gmail.com>
2020-04-09 17:33:23 +0200

    Only handle file dragging and not text/other. Fixes #209 Add check for drag and drop support before adding events

--- js/src/forum/components/DragAndDrop.js
+++ js/src/forum/components/DragAndDrop.js
@@ -6,6 +6,10 @@ export default class DragAndDrop {
         // Keep references to the bound methods so we can remove the event listeners later
         this.handlers = {};
 
+        if (!this.supportsFileDragging()) {
+            return;
+        }
+
         this.composerElement.addEventListener('dragover', this.handlers.in = this.in.bind(this));
 
         this.composerElement.addEventListener('dragleave', this.handlers.out = this.out.bind(this));
@@ -16,7 +20,19 @@ export default class DragAndDrop {
         this.isDropping = this.over = false;
     }
 
+    supportsFileDragging() {
+        // Based on https://css-tricks.com/drag-and-drop-file-uploading/
+        const div = document.createElement('div');
+
+        return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+    }
+
     unload() {
+        // If the handlers were not set (drag and drop not supported), we skip removing them
+        if (!this.handlers.in) {
+            return;
+        }
+
         this.composerElement.removeEventListener('dragover', this.handlers.in);
 
         this.composerElement.removeEventListener('dragleave', this.handlers.out);
@@ -25,7 +41,25 @@ export default class DragAndDrop {
         this.composerElement.removeEventListener('drop', this.handlers.dropping);
     }
 
+    isNotFile(event) {
+        // Checking event.dataTransfer.files.length does not work on dragover event, it's always zero
+        // So we use the dataTransfer.items property to check whether any file is being dragged
+        if (event.dataTransfer.items) {
+            for (let i = 0; i < event.dataTransfer.items.length; i++) {
+                if (event.dataTransfer.items[i].kind !== 'file') {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
     in(event) {
+        if (this.isNotFile(event)) {
+            return;
+        }
+
         event.preventDefault();
 
         if (!this.over) {
@@ -35,6 +69,10 @@ export default class DragAndDrop {
     }
 
     out(event) {
+        if (this.isNotFile(event)) {
+            return;
+        }
+
         event.preventDefault();
 
         if (this.over) {
@@ -44,6 +82,10 @@ export default class DragAndDrop {
     }
 
     dropping(event) {
+        if (this.isNotFile(event)) {
+            return;
+        }
+
         event.preventDefault();
 
         if (!this.isDropping) {
