fdbe86142617ddf696416d5687c90d194b8ea605
Ian Morland <ianm@giffgaff.co.uk>
2020-05-04 11:19:30 +0100

    Replace Zend with Laminas, update core version constraints

--- src/Api/Controllers/UploadController.php
+++ src/Api/Controllers/UploadController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Collection;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Zend\Diactoros\Response\JsonResponse;
+use Laminas\Diactoros\Response\JsonResponse;
 
 class UploadController implements RequestHandlerInterface
 {
