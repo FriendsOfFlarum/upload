08b6ce4e5832883fefeeeb6bbc04ad93b1c82da5
Ian Morland <ianm@giffgaff.co.uk>
2020-05-08 11:16:11 +0100

    Check if post exists before applying foriiegn key constraint

--- migrations/2020_02_06_07_alter_downloads_add_post_constraint.php
+++ migrations/2020_02_06_07_alter_downloads_add_post_constraint.php
@@ -5,6 +5,15 @@ use Illuminate\Database\Schema\Builder;
 
 return [
     'up' => function (Builder $schema) {
+        // Set post_id null if the post does not exist.
+        // Will prevent issues when applying foriegn key constraint.
+        $connection = $schema->getConnection();
+        $connection->table('fof_upload_downloads')
+            ->whereNotExists(function ($query) {
+                $query->selectRaw(1)->from('posts')->whereColumn('post_id', 'id');
+            })
+            ->update(['post_id' => null]);
+
         $schema->table('fof_upload_downloads', function (Blueprint $table) {
             $table->foreign('post_id')
                 ->references('id')
