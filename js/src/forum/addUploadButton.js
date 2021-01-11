8aa7b76eb40bae90dfb8db8d93edc2ef91be4373
David Wheatley <davidwheatley03@gmail.com>
2021-01-11 18:51:55 +0000

    Don't attempt to preview the uploaded content when creating a new thread (#255)

--- js/src/forum/addUploadButton.js
+++ js/src/forum/addUploadButton.js
@@ -27,16 +27,21 @@ export default function () {
         this.uploader.on('success', (image) => {
             this.attrs.composer.editor.insertAtCursor(image + '\n');
 
-            // Scroll the preview into view
-            // preview() causes the composer to close on mobile, but we don't want that. We want only the scroll
-            // We work around that by temporarily patching the isFullScreen method
-            const originalIsFullScreen = app.composer.isFullScreen;
+            // We wrap this in a typeof check to prevent it running when a user
+            // is creating a new discussion. There's nothing to preview in a new
+            // discussion, so the `preview` function isn't defined.
+            if (typeof this.attrs.preview === 'function') {
+                // Scroll the preview into view
+                // preview() causes the composer to close on mobile, but we don't want that. We want only the scroll
+                // We work around that by temporarily patching the isFullScreen method
+                const originalIsFullScreen = app.composer.isFullScreen;
 
-            app.composer.isFullScreen = () => false;
+                app.composer.isFullScreen = () => false;
 
-            this.attrs.preview();
+                this.attrs.preview();
 
-            app.composer.isFullScreen = originalIsFullScreen;
+                app.composer.isFullScreen = originalIsFullScreen;
+            }
         });
 
         const dragAndDrop = new DragAndDrop((files) => this.uploader.upload(files), this.$().parents('.Composer')[0]);
