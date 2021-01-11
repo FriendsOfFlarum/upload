0bee22feca4fbe4b2b305d87096be5253b475510
Ian Morland <imorland@users.noreply.github.com>
2021-01-11 18:46:51 +0000

    Apply fixes from StyleCI

--- src/Formatter/ReplaceDeprecatedTemplates.php
+++ src/Formatter/ReplaceDeprecatedTemplates.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of fof/upload.
  *
- * Copyright (c) 2020 FriendsOfFlarum.
+ * Copyright (c) 2020 - 2021 FriendsOfFlarum.
  * Copyright (c) 2016 - 2019 Flagrow
  *
  * For the full copyright and license information, please view the LICENSE.md
