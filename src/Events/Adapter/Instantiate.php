0afa4985889c6d732607eedcb83b2e9d7165679e
luceos <luceos@users.noreply.github.com>
2021-01-20 14:00:34 +0000

    Apply fixes from StyleCI

--- src/Events/Adapter/Instantiate.php
+++ src/Events/Adapter/Instantiate.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of fof/upload.
+ *
+ * Copyright (c) 2020 - 2021 FriendsOfFlarum.
+ * Copyright (c) 2016 - 2019 Flagrow
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
 namespace FoF\Upload\Events\Adapter;
 
 use FoF\Upload\Helpers\Util;
