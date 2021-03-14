08079568df367ca6717fc323e98ee43b7b202692
David Wheatley <hi@davwheat.dev>
2021-03-14 01:54:46 +0000

    Add JSDoc comment for mimeToIcon

--- js/src/common/mimeToIcon.js
+++ js/src/common/mimeToIcon.js
@@ -9,6 +9,9 @@ const word = [
 ];
 
 /**
+ * Returns a FontAwesome icon class for a specified MIME type.
+ *
+ * If it's a known image type, it returns `image` instead.
  *
  * @param {string} fileType MIME type
  * @returns {string} Icon for MIME type
