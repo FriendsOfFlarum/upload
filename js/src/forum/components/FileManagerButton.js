73b81086e64b4498c2e200f325cd3572ddf27332
Ian Morland <ian@morland.me>
2021-05-20 10:02:52 +0100

    Replace media manager button tooltip

--- js/src/forum/components/FileManagerButton.js
+++ js/src/forum/components/FileManagerButton.js
@@ -2,27 +2,19 @@ import app from 'flarum/common/app';
 import Component from 'flarum/common/Component';
 import Button from 'flarum/common/components/Button';
 import FileManagerModal from './FileManagerModal';
+import Tooltip from 'flarum/common/components/Tooltip';
 
 export default class FileManagerButton extends Component {
     view() {
-        return Button.component({
-            className: 'Button fof-upload-button Button--icon',
-            onclick: this.fileManagerButtonClicked.bind(this),
-            icon: 'fas fa-photo-video',
-            title: app.translator.trans('fof-upload.forum.buttons.media'),
-        });
-    }
-
-    /**
-     * Show tooltip on hover
-     *
-     * @param {*} vnode
-     */
-    oncreate(vnode) {
-        super.oncreate(vnode);
-
-        // Add tooltip
-        this.$().tooltip();
+        return (
+            <Tooltip text={app.translator.trans('fof-upload.forum.buttons.media')}>
+                {Button.component({
+                    className: 'Button fof-upload-button Button--icon',
+                    onclick: this.fileManagerButtonClicked.bind(this),
+                    icon: 'fas fa-photo-video',
+                })}
+            </Tooltip>
+        );
     }
 
     /**
