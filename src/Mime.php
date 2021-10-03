b39452933337752678dca63d503cc9724be308ae
Ian Morland <imorland@users.noreply.github.com>
2021-10-03 11:18:54 +0000

    Apply fixes from StyleCI

--- src/Mime.php
+++ src/Mime.php
@@ -1,11 +1,21 @@
 <?php
 
+/*
+ * This file is part of fof/upload.
+ *
+ * Copyright (c) FriendsOfFlarum.
+ * Copyright (c) Flagrow.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace FoF\Upload;
 
 use Flarum\Database\AbstractModel;
 
 /**
- * @property int $id
+ * @property int    $id
  * @property string $adapter
  * @property string $template
  * @property string $regex
