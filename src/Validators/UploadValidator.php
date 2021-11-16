e373f1e31af5028a842c21acc77ac42c3629e99d
Ian Morland <ian@morland.me>
2021-11-16 22:24:09 +0000

    Translatable file over size message

--- src/Validators/UploadValidator.php
+++ src/Validators/UploadValidator.php
@@ -15,19 +15,37 @@ namespace FoF\Upload\Validators;
 use Flarum\Foundation\AbstractValidator;
 use Flarum\Settings\SettingsRepositoryInterface;
 use FoF\Upload\Helpers\Util;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class UploadValidator extends AbstractValidator
 {
     protected function getRules(): array
     {
-        /** @var SettingsRepositoryInterface $settings */
-        $settings = resolve(SettingsRepositoryInterface::class);
-
         return [
             'file' => [
                 'required',
-                'max:'.$settings->get('fof-upload.maxFileSize', Util::DEFAULT_MAX_FILE_SIZE),
+                'max:' . $this->maxFilesize(),
             ],
         ];
+    }
+
+    protected function getMessages()
+    {
+        /** @var TranslatorInterface $translator */
+        $translator = resolve('translator');
+
+        return [
+            'max' => $translator->trans('fof-upload.forum.validation.max_size', [
+                'max' => $this->maxFilesize()
+            ])
+        ];
+    }
+
+    protected function maxFilesize(): int
+    {
+        /** @var SettingsRepositoryInterface $settings */
+        $settings = resolve(SettingsRepositoryInterface::class);
+
+        return $settings->get('fof-upload.maxFileSize', Util::DEFAULT_MAX_FILE_SIZE);
     }
 }
