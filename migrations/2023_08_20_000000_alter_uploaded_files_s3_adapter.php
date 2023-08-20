463a963fc428d4a8b4f50e6bc9faedfe9e1b3985
Daniël Klabbers <daniel.klabbers@cloudmazing.nl>
2023-08-20 09:28:04 +0200

    fix: s3 adapters arent migrated to 3.x Manager

---
+++ migrations/2023_08_20_000000_alter_uploaded_files_s3_adapter.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of fof/upload.
+ *
+ * Copyright (c) FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Illuminate\Database\Schema\Builder;
+
+function setAwsUploadedFilesAdapterDelimiter(Builder $schema, string $old = '_', string $new = '-')
+{
+    $schema
+        ->getConnection()
+        ->table('fof_upload_files')
+        ->where('upload_method', "aws{$old}s3")
+        ->update(['upload_method' => "aws{$new}s3"]);
+}
+
+return [
+    'up' => function (Builder $schema) {
+        setAwsUploadedFilesAdapterDelimiter($schema, '_', '-');
+    },
+    'down' => function (Builder $schema) {
+        setAwsUploadedFilesAdapterDelimiter($schema, '-', '_');
+    },
+];
