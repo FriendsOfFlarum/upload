eebb4c3e7bdb07c2302c354efa3c9f4c50db941f
Daniël Klabbers <daniel.klabbers@cloudmazing.nl>
2023-08-20 09:23:16 +0200

    fix: s3 adapters arent migrated to 3.x Manager

---
+++ migrations/2023_08_20_000000_alter_uploads_s3_adapter.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of fof/upload.
+ *
+ * Copyright (c) FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Illuminate\Database\Schema\Builder;
+
+function setAwsMimeTypeAdapterDelimiter(Builder $schema, string $old = '_', string $new = '-')
+{
+
+    $mimeConfiguration = $schema
+        ->getConnection()
+        ->table('settings')
+        ->where('key', 'fof-upload.mimeTypes')
+        ->value('value');
+
+    $mimeConfiguration = json_decode($mimeConfiguration, true);
+
+    foreach ($mimeConfiguration as $mime => &$config) {
+        if ($config['adapter'] === "aws{$old}s3") $config['adapter'] = "aws{$new}s3";
+    }
+    
+    $schema
+        ->getConnection()
+        ->table('settings')
+        ->where('key', 'fof-upload.mimeTypes')
+        ->update(['value' => json_encode($mimeConfiguration)]);
+}
+
+return [
+    'up' => function (Builder $schema) {
+        setAwsMimeTypeAdapterDelimiter($schema, '_', '-');
+    },
+    'down' => function (Builder $schema) {
+        setAwsMimeTypeAdapterDelimiter($schema, '-', '_');
+    },
+];
