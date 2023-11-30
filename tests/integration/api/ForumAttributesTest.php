4f53ead0b13900070a7a236088596e20bfa4beb2
IanM <16573496+imorland@users.noreply.github.com>
2023-11-30 15:13:51 +0000

    chore: create first basic tests (#365)
    * chore:basic serializer tests
    
    * Apply fixes from StyleCI
    
    ---------
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

---
+++ tests/integration/api/ForumAttributesTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of fof/upload.
+ *
+ * Copyright (c) FriendsOfFlarum.
+ * Copyright (c) Flagrow.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FoF\Upload\Tests\integration\api;
+
+use Flarum\Testing\integration\TestCase;
+
+class ForumAttributesTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->extension('fof-upload');
+    }
+
+    /**
+     * @test
+     */
+    public function upload_attributes_are_added_to_forum()
+    {
+        $response = $this->send(
+            $this->request(
+                'GET',
+                '/api'
+            )
+        );
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $json = json_decode($response->getBody()->getContents(), true);
+
+        $this->assertArrayHasKey('fof-upload.canUpload', $json['data']['attributes']);
+        $this->assertArrayHasKey('fof-upload.canDownload', $json['data']['attributes']);
+        $this->assertArrayHasKey('fof-upload.composerButtonVisiblity', $json['data']['attributes']);
+    }
+}
---
+++ tests/integration/setup.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of fof/upload.
+ *
+ * Copyright (c) FriendsOfFlarum.
+ * Copyright (c) Flagrow.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Flarum\Testing\integration\Setup\SetupScript;
+
+require __DIR__.'/../../vendor/autoload.php';
+
+$setup = new SetupScript();
+
+$setup->run();
