e8050f045f62649bb45e9a4a7651b16316439b7c
IanM <ian@morland.me>
2023-12-01 11:02:31 +0000

    add basic upload file test

---
+++ tests/integration/api/FileUploadTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace FoF\Upload\Tests\integration\api;
+
+use Flarum\Testing\integration\RetrievesAuthorizedUsers;
+use FoF\Upload\Tests\EnhancedTestCase;
+
+class FileUploadTest extends EnhancedTestCase
+{
+    use RetrievesAuthorizedUsers;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->extension('fof-upload');
+
+        $this->prepareDatabase([
+            'users' => [
+                $this->normalUser(),
+            ],
+        ]);
+    }
+
+    protected function giveNormalUserUploadPermission()
+    {
+        $this->prepareDatabase(
+            [
+                'group_permission' => [
+                    ['group_id' => 3, 'permission' => 'fof-upload.upload'],
+                ],
+            ]
+        );
+    }
+
+    protected function uploadFile(string $path)
+    {
+        if (!file_exists($path)) {
+            throw new \InvalidArgumentException("File not found at path: $path");
+        }
+
+        return [
+            'name' => 'files',
+            'contents' => $path,
+            'filename' => basename($path),
+        ];
+    }
+
+
+    protected function fixtures(string $file): string
+    {
+        return __DIR__ . '/../../fixtures/' . $file;
+    }
+
+    /**
+     * @test
+     */
+    public function user_with_permission_can_upload_a_file()
+    {
+        $this->giveNormalUserUploadPermission();
+
+        $response = $this->send(
+            $this->request('POST', '/api/fof/upload', [
+                'authenticatedAs' => 2,
+                'multipart' => [
+                    $this->uploadFile($this->fixtures('MilkyWay.jpg')),
+                ],
+            ])
+        );
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $json = json_decode($response->getBody()->getContents(), true);
+
+        $this->assertArrayHasKey('data', $json);
+        $this->assertCount(1, $json['data']);
+        $this->assertArrayHasKey('attributes', $json['data'][0]);
+
+        $this->assertEquals('milkyway.jpg', $json['data'][0]['attributes']['baseName']);
+        $this->assertEquals('image/jpeg', $json['data'][0]['attributes']['type']);
+        $this->assertEquals('image-preview', $json['data'][0]['attributes']['tag']);
+    }
+
+    /**
+     * @test
+     */
+    public function user_without_permission_cannot_upload_a_file()
+    {
+        $response = $this->send(
+            $this->request('POST', '/api/fof/upload', [
+                'authenticatedAs' => 2,
+                'multipart' => [
+                    $this->uploadFile($this->fixtures('MilkyWay.jpg')),
+                ],
+            ])
+        );
+
+        $this->assertEquals(403, $response->getStatusCode());
+    }
+}
