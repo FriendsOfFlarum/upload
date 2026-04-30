1ca085940bf4d2dc958e33f586cc2e0dd0c6cd9c
Rafał Całka <25438601+rafaucau@users.noreply.github.com>
2026-04-30 13:18:07 +0200

    Remove unnecessary newline in image tag

--- resources/templates/image-preview.blade.php
+++ resources/templates/image-preview.blade.php
@@ -1,2 +1,1 @@
-<img class="FoFUpload--Upl-Image-Preview" src="{@url}" title="{@title}" alt="{@alt}" data-id="{@uuid}" loading="lazy"
-    width="{@width}" height="{@height}" />
+<img class="FoFUpload--Upl-Image-Preview" src="{@url}" title="{@title}" alt="{@alt}" data-id="{@uuid}" loading="lazy" width="{@width}" height="{@height}"/>
