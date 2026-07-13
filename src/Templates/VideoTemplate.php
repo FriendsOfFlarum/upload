2e613e341b42817640c9e51b46322d6657e41aee
u0_ani-nya.com <89579156+u0-ani-nya@users.noreply.github.com>
2026-07-13 15:05:43 +0800

    Update VideoTemplate.php

--- src/Templates/VideoTemplate.php
+++ src/Templates/VideoTemplate.php
@@ -1,6 +1,6 @@
 public function template(): string
 {
     return app('view')->make('fof-upload::templates.video', [
-        'url' => $this->url // 这里把 PHP 的变量传给 Blade
+        'url' => $this->url 
     ])->render();
 }
