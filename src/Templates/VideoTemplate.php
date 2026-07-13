d463b8bbf754fc042950b64c5178ed17ae7442fd
u0_ani-nya.com <89579156+u0-ani-nya@users.noreply.github.com>
2026-07-13 15:22:10 +0800

    Refactor VideoTemplate class methods and properties

--- src/Templates/VideoTemplate.php
+++ src/Templates/VideoTemplate.php
@@ -1,6 +1,47 @@
-public function template(): string
+<?php
+
+namespace FoF\Upload\Templates;
+
+use Illuminate\Contracts\View\View;
+
+class VideoTemplate extends AbstractTextFormatterTemplate
 {
-    return app('view')->make('fof-upload::templates.video', [
-        'url' => $this->url 
-    ])->render();
+    /**
+     * @var string
+     */
+    protected $tag = 'video-player';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function name(): string
+    {
+        // 如果你还没加语言包，可以直接写 'Video Player'，或者使用 $this->trans()
+        return 'Video Player';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function description(): string
+    {
+        return 'Display as a video player';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function template(): View
+    {
+        // 对应资源目录下的 video.blade.php
+        return $this->getView('fof-upload.templates::video');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function bbcode(): string
+    {
+        return '[video]{url}[/video]';
+    }
 }
