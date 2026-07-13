0f7e2cd4c24f75e18a76fde8424bf7cc568ff1a8
u0_ani-nya.com <89579156+u0-ani-nya@users.noreply.github.com>
2026-07-13 16:17:44 +0800

    Update video.blade.php

--- resources/templates/video.blade.php
+++ resources/templates/video.blade.php
@@ -1,6 +1,5 @@
 <div class="fof-upload-video">
-    <!-- 此时解析器就认识 @video 了 -->
-    <video controls style="width: 100%; border-radius: 8px;" title="{@video}">
+    <video controls style="width: 100%; border-radius: 8px;" title="{@upl-video}">
         <source src="{@url}">
         Your Browser Don't support Video player.
     </video>
