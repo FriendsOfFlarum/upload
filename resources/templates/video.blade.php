c9d2ccf4da77ca945056f27210660d8c4732cda2
u0_ani-nya.com <89579156+u0-ani-nya@users.noreply.github.com>
2026-07-13 15:38:20 +0800

    Update video.blade.php

--- resources/templates/video.blade.php
+++ resources/templates/video.blade.php
@@ -1,5 +1,5 @@
 <div class="fof-upload-video">
     <video controls style="width: 100%; border-radius: 8px;">
-        <source src="{@url}" type="video/mp4">
+        <source src="{@video}" type="video/mp4">
     </video>
 </div>
