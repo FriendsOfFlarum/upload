018cb13efdef20616b2b61a4ae2d32c65cba053b
u0_ani-nya.com <89579156+u0-ani-nya@users.noreply.github.com>
2026-07-13 16:09:15 +0800

    Update VideoTemplate.php

--- src/Templates/VideoTemplate.php
+++ src/Templates/VideoTemplate.php
@@ -40,6 +40,6 @@ class VideoTemplate extends AbstractTextFormatterTemplate
      */
     public function bbcode(): string
     {
-        return '[upl-video uuid={IDENTIFIER} url={URL}]{SIMPLETEXT1}[/upl-video]';
+        return '[upl-video uuid={IDENTIFIER} url={URL}]{VIDEO}[/upl-video]';
     }
 }
