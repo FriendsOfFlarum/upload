d055f9059fa8c397c2b061ad14db6cce696d212a
u0_ani-nya.com <89579156+u0-ani-nya@users.noreply.github.com>
2026-07-13 15:05:34 +0800

    Update VideoTemplate to render using Blade
    Refactor VideoTemplate class to use Blade templating.

--- src/Templates/VideoTemplate.php
+++ src/Templates/VideoTemplate.php
@@ -1,28 +1,6 @@
-<?php
-
-namespace FoF\Upload\Templates;
-
-use FoF\Upload\File;
-
-class VideoTemplate extends AbstractTemplate
+public function template(): string
 {
-    public function name(): string
-    {
-        return 'Video Player'; // 界面显示的名字
-    }
-
-    public function tag(): string
-    {
-        return 'video-player';
-    }
-
-    public function template(): string
-    {
-        return '<video controls preload="metadata" style="max-width:100%"><source src="{@url}"></video>';
-    }
-
-    public function bbcode(): string
-    {
-        return '[video]{url}[/video]';
-    }
+    return app('view')->make('fof-upload::templates.video', [
+        'url' => $this->url // 这里把 PHP 的变量传给 Blade
+    ])->render();
 }
