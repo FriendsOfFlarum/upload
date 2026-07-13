905f53079620b76e74311ef78839d74bea9447c0
u0_ani-nya.com <89579156+u0-ani-nya@users.noreply.github.com>
2026-07-13 16:23:29 +0800

    Update video.blade.php

--- resources/templates/video.blade.php
+++ resources/templates/video.blade.php
@@ -1,6 +1,7 @@
 <div class="fof-upload-video">
     <video controls style="width: 100%; border-radius: 8px;">
         <source src="{@url}">
-        Your Browser Don't support Video player.
+         Your Browser Don't support Video player.
     </video>
+    <span hidden><xsl:apply-templates/></span>
 </div>
